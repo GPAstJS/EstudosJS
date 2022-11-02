9bd23a65316e78396ed1b074a5cb9e6af8fc3f7f
GPAstJS <gpastjs22@gmail.com>
2022-11-01 22:27:07 -0300

    created my first modal

---
+++ HTML-CSS-JS/modal/main.js
@@ -0,0 +1,15 @@
+const modal = document.getElementById('id-modal')
+const header = document.getElementById('id-header')
+
+const openModal = document.getElementById('open-btn');
+const closeModal = document.getElementById('close-btn');
+
+function showModal() {
+    modal.style.display = 'block'
+    document.body.style.backgroundColor = "#6C757D";
+}
+
+function hideModal() {
+    modal.style.display = 'none'
+    document.body.style.backgroundColor = '#fff'
+}
