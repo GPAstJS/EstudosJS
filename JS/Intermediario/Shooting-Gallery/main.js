656604385842144c930873c60362114f281fce5c
GPAstJS <gpastjs22@gmail.com>
2022-11-03 12:35:24 -0300

    shooting gallery

---
+++ JS/Intermediario/Shooting-Gallery/main.js
@@ -0,0 +1,17 @@
+let circle = document.getElementById('circle')
+
+document.addEventListener("mousemove", (event) => {
+    const {clientX, clientY} = event;
+    console.log(clientX, clientY);
+});
+
+
+window.addEventListener('click', (e) => {
+    circle = event
+})
+
+
+
+function distanciaEntre() {
+        
+}
