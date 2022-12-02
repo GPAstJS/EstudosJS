0815432a4d777de2657bf9020cd9285e713fafea
GPAstJS <gpastjs22@gmail.com>
2022-12-01 22:34:25 -0300

    dashboard com json finalizado

--- HTML-CSS-JS/dashboard/main.js
+++ HTML-CSS-JS/dashboard/main.js
@@ -1,71 +1,40 @@
 // armazenando os botoes
 
-const dailyBtn = document.getElementById('daily-btn');
-const weeklyBtn = document.getElementById('weekly-btn');
-const monthlyBtn = document.getElementById('monthly-btn');
+const dailyBtn = document.getElementById("daily-btn");
+const weeklyBtn = document.getElementById("weekly-btn");
+const monthlyBtn = document.getElementById("monthly-btn");
 
 // armazenando os headings pa
 
-const h2tag = document.getElementsByClassName('h2tag')
-const h4tag = document.getElementsByClassName('h4tag');
-
-
-
+const h2tag = document.getElementsByClassName("h2tag");
+const h4tag = document.getElementsByClassName("h4tag");
 
 async function Api() {
-    const response = await fetch('http://localhost:3000/data')
-  
-    const data = await response.json();
-  
-    console.log('Retorno do Fetch: ', data);
-  
-    return data;
-  }
-  
-   const data = Api();
-   console.log(data )
+  const response = await fetch("http://localhost:3000/data");
 
+  const data = await response.json();
 
-   dailyBtn.addEventListener('click', (e) => {
-
-    const dailyLetras = ['a', 'b', 'c', 'd', 'e', 'f']
-    const dailyNumbers = ['1', '2', '3', '4', '5', '6' ]
-
-    for(i = 0; i < h2tag.length; i++) {
-        h2tag[i].innerHTML = dailyLetras[i]
+  dailyBtn.addEventListener("click", (e) => {
+    for(i = 0; i < data.length; i++) {
+      h2tag[i].innerHTML = `${data[i].timeframes.daily.current}hrs`
+      h4tag[i].innerHTML = `Previous - ${data[i].timeframes.daily.previous}hrs`
     }
+  });
 
-    for(j = 0; j < h4tag.length; j++) {
-        h4tag[j].innerHTML = dailyNumbers[j]  
+  weeklyBtn.addEventListener('click', (e) => {
+    for(i = 0; i < data.length; i++) {
+      h2tag[i].innerHTML = `${data[i].timeframes.weekly.current}hrs`;
+      h4tag[i].innerHTML = `Previous - ${data[i].timeframes.weekly.previous}hrs`
     }
-})
+  })
 
-weeklyBtn.addEventListener('click', (e) => {
-
-        
-    const weeklyLetras = ['g', 'h', 'i', 'j', 'k', 'l']
-    const weeklyNumbers = ['7', '8', '9', '10', '11', '12']
-
-    for(i = 0; i < h2tag.length; i++) {
-        h2tag[i].innerHTML = weeklyLetras[i]
+  monthlyBtn.addEventListener('click', (e) => {
+    for(i = 0; i < data.length; i++) {
+      h2tag[i].innerHTML = `${data[i].timeframes.monthly.current}hrs`;
+      h4tag[i].innerHTML = `Previous - ${data[i].timeframes.monthly.previous}hrs`
     }
+  })
 
-    for(j = 0; j < h4tag.length; j++) {
-        h4tag[j].innerHTML = weeklyNumbers[j]  
-    }
-})
+}
 
-monthlyBtn.addEventListener('click', (e) => {
-    const monthlyLetras = ['m', 'n', 'o', 'p', 'q', 'r']
-    const monthlyNumbers = ['13','14','15','16','17', '18']
-
-    for(i = 0; i < h2tag.length; i++) {
-        h2tag[i].innerHTML = monthlyLetras[i]
-    }
-
-    for(j = 0; j < h4tag.length; j++) {
-        h4tag[j].innerHTML = monthlyNumbers[j]  
-    }
-})
-
-
+Api();
