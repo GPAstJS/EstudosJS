e62bfa28a9a5e833928802870ed760c71e61eb6b
GPAstJS <gpastjs22@gmail.com>
2023-01-15 23:55:55 -0300

    first calculator bugfree

--- HTML-CSS-JS/calculadora/main.js
+++ HTML-CSS-JS/calculadora/main.js
@@ -1,23 +1,12 @@
-// const botoes = document.getElementsByClassName('botoes')
-// const visor = document.getElementById('visor')
-
-// for(i = 0; i < botoes.length; i++) {
-//     botoes[i].addEventListener('click', (e) => {
-//         switch(e.target.value) {
-//             case '=': visor.value = eval(visor.value)
-//             default: visor.value = visor.value + e.target.value
-//         }
-//     })
-// }
-
 const buttons = document.getElementsByClassName("botoes");
 const visor = document.getElementById("visor");
 
 const equalsBtn = document.getElementById("equals");
 const cleanBtn = document.getElementById("clean");
 
-const historico = document.getElementById('historico')
+const historico = document.getElementById("historico");
 
+let tamanhoDoVisor;
 let verificador = false;
 
 function limpar() {
@@ -25,19 +14,25 @@ function limpar() {
   visor.value = "";
 }
 
-// 
-
 function calcular() {
   verificador = true;
-  historico.innerHTML = historico.innerHTML +  visor.value + '<br>'
 
-  try {
-    visor.value = eval(visor.value);
-  } catch (error) {
-    alert("Insira uma operação matemática que faça sentido!");
-    visor.value = "";
-  }
+  valoresDentroDoVisor = visor.value.split("");
 
+  const visorFiltrado = valoresDentroDoVisor.filter(
+    (el) => el == "+" || el == "-" || el == "/" || el == "*"
+  );
+
+  if (visorFiltrado.length >= 1 && !isNaN(visor.value[0])) {
+    try {
+      visor.value = eval(visor.value);
+      historico.innerHTML = historico.innerHTML + visor.value + "<br>";
+      
+    } catch (error) {
+      alert("Insira uma operação matemática que faça sentido!");
+      visor.value = "";
+    }
+  } else alert("Por favor, insira um operando!");
 }
 
 for (i = 0; i < buttons.length; i++) {
---
+++ HTML-CSS-JS/calculadora/readme.md
@@ -0,0 +1,9 @@
+ * AUTHOR: GPAstJS
+
+
+ * NAME PROJECT: Calculator JS
+ * PACKAGE: 0.1
+ * VERSION: 0.1
+
+
+ * DESCRIPTION: A little calculator for JS based on HTML. 
